6015fcfacdf5ade6ec25075511a55bd73bcfe28f
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2024-02-12 22:10:04 +0530

    Using stack navigator for example screens
    -> Convert normal example screens to load as stack screens

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 import Example2 from './src/screens/Example2/index';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
@@ -8,6 +13,15 @@ export default function App() {
     // <HomeScreen />
 
     // Example 2
-    <Example2 />
+    // <Example2 />
+
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Stack.Navigator>
+          <Stack.Screen name='One' component={HomeScreen} />
+          <Stack.Screen name='Two' component={Example2} />
+        </Stack.Navigator>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
